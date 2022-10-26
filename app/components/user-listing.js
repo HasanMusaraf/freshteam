bc6384abe17f4cd1bb8f6f81d780ad38e1b11dc7
Mohamed Hasan Musaraf <mhasanmusaraf@MacBook-Pro-7.local>
2022-10-26 15:11:36 +0530

    showing users list upto g completed

--- app/components/user-listing.js
+++ app/components/user-listing.js
@@ -1,14 +1,22 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+// import { computed } from '@ember/object';
+
 
 export default Component.extend({
     store: service(),
     init() {
         this._super(...arguments);
-        this.getUser = this.get('store').peekAll('user'); 
-        
+        this.getUser = this.get('store').findAll('user'); 
+    },
 
-    }
+    // getRandomColor: computed('randomColor',function(){
+    //     this.randomColor = ['#9FDEDA','black','red','green','blue']
+    //     this.randomColor.forEach(function (color){
+    //       return color
+    //     })
+    //   })
+    
 
     
 });
--- mirage/serializers/application.js
+++
@@ -1,5 +0,0 @@
-
-import LSSerializer from 'freshteam/mirage/serializers/ls-serializer';
-
-export default LSSerializer.extend({
-});
