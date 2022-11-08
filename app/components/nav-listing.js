2ba71d2c8d394615579b4c2991e684cf5cab932c
Mohamed Hasan Musaraf <mhasanmusaraf@MacBook-Pro-7.local>
2022-11-08 16:04:58 +0530

    task4 subtask C

--- app/components/nav-listing.js
+++ app/components/nav-listing.js
@@ -1,11 +1,11 @@
-import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
+import Component from "@ember/component";
+import { inject as service } from "@ember/service";
+import { computed } from "@ember/object";
 
 export default Component.extend({
   router: service(),
 
-  isUserRoute: computed('router.currentRouteName', function () {
-    return this.router.currentRouteName === 'users';
+  isUserRoute: computed("router.currentRouteName", function () {
+    return this.router.currentRouteName === "users.index" || this.router.currentRouteName === "users.new";
   }),
 });
--- app/components/slide-modal.js
+++ app/components/slide-modal.js
@@ -1,13 +1,12 @@
 import Component from "@ember/component";
-import { inject as service } from '@ember/service';
-
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
   router: service(),
 
   actions: {
     close: function () {
-      this.router.transitionTo('users')
+      this.router.transitionTo("users");
     },
   },
 });
