d9198f9ea9558205494a4f5296fb582eafd565a5
Mohamed Hasan Musaraf <mhasanmusaraf@MacBook-Pro-7.local>
2022-11-10 18:57:21 +0530

    bs-datepicker chamnges

--- app/components/user-form.js
+++ app/components/user-form.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    changeDateAction: function(d) {
+      // do sth with the new date
+      console.log(d.getDate());
+    },
+  }
 });
