8ba3f9c7395a544aecf7289a2b0df1bdf6dc1d9f
Mohamed Hasan Musaraf <mhasanmusaraf@MacBook-Pro-7.local>
2022-11-13 16:33:40 +0530

    ember power select changes

--- app/components/user-form.js
+++ app/components/user-form.js
@@ -1,10 +1,22 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+init(){
+  this._super(...arguments);
+  this.cities = ['FreshTeam', 'FreshSales', 'FreshChat', 'FreshBot'];
+  this.destination = 'London'
+
+},
+
   actions: {
     changeDateAction: function(d) {
       // do sth with the new date
       console.log(d.getDate());
     },
+    chooseDestination(city) {
+      this.set('destination', city);
+    }
   }
+
+  
 });
