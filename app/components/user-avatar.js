cfd02f1e1b15caaf7b3129d005d2c96f6b9cf07a
HasanMusaraf <93640404+HasanMusaraf@users.noreply.github.com>
2022-11-15 18:48:25 +0530

    Revert "task-3C temp file changes"
    This reverts commit c0cd0975f1cfa8f4be0265d05875f7119ee2ef75.

--- app/components/user-avatar.js
+++ app/components/user-avatar.js
@@ -8,14 +8,14 @@ export default Component.extend({
 
   init() {
     this._super(...arguments);
-    this.profileChar = this.user.firstName.charAt(0);
+    this.profile_char = this.user.first_name.charAt(0);
     this.avatar_colors = ['#AFCFFD', '#E1C8CE', '#ABCFE0', '#9FDEDA', '#FCC5B7', '#F1BBE7', '#E2C1DF', '#A2C7F6', '#D2CDE0', '#C7D9A8', '#95D1F3',
       '#BFC5EE', '#E8BBC1', '#DDC9B8', '#AFCFFD', '#ABCFE0', '#9FDEDA', '#FCC5B7', '#F1BBE7', '#E2C1DF', '#A2C7F6', '#D2CDE0',
       '#C7D9A8', '#95D1F3', '#BFC5EE', '#E8BBC1'];
   },
 
   isWork: function () {
-    const charCodes = this.profileChar.charCodeAt();
+    const charCodes = this.profile_char.charCodeAt();
     return parseInt(charCodes, 10)
 
   },
--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
+  first_name: DS.attr('string'),
+  last_name: DS.attr('string'),
   email: DS.attr('string'),
   team: DS.attr('string'),
   joiningDate: DS.attr('date'),
