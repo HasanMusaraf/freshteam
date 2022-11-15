5c0c9e0b8f501c698b9d2e3f58016c72d85c3afc
Mohamed Hasan Musaraf <mhasanmusaraf@MacBook-Pro-7.local>
2022-11-15 13:04:32 +0530

    task2 subtask e PR comments resolved

--- app/components/user-avatar.js
+++ app/components/user-avatar.js
@@ -8,20 +8,20 @@ export default Component.extend({
 
   init() {
     this._super(...arguments);
-    this.profile_char = this.user.first_name.charAt(0);
+    this.profileChar = this.user.firstName.charAt(0);
     this.avatar_colors = ['#AFCFFD', '#E1C8CE', '#ABCFE0', '#9FDEDA', '#FCC5B7', '#F1BBE7', '#E2C1DF', '#A2C7F6', '#D2CDE0', '#C7D9A8', '#95D1F3',
       '#BFC5EE', '#E8BBC1', '#DDC9B8', '#AFCFFD', '#ABCFE0', '#9FDEDA', '#FCC5B7', '#F1BBE7', '#E2C1DF', '#A2C7F6', '#D2CDE0',
       '#C7D9A8', '#95D1F3', '#BFC5EE', '#E8BBC1'];
   },
 
-  isWork: function () {
-    const charCodes = this.profile_char.charCodeAt();
+  getCharCode: function () {
+    const charCodes = this.profileChar.charCodeAt();
     return charCodes
 
   },
 
-  isClosing: computed('avatar_colors', function () {
-      return htmlSafe("background: " + this.avatar_colors[this.isWork() % this.avatar_colors.length]);
+  getBgColor: computed('avatar_colors', function () {
+      return htmlSafe("background: " + this.avatar_colors[this.getCharCode() % this.avatar_colors.length]);
 
   }),
 });
