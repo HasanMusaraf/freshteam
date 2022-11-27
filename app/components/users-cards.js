4c715a9d189110ee3637b623b7fce86a77882c01
HasanMusaraf <93640404+HasanMusaraf@users.noreply.github.com>
2022-11-27 15:51:33 +0530

    Task3 subtask a and b Filter functionality changed

--- app/components/users-cards.js
+++ app/components/users-cards.js
@@ -1,4 +1,36 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { computed, get, set } from '@ember/object';
 
 export default Component.extend({
+  store: service(),
+
+  allUsers: computed(function () {
+    return get(this,'store').findAll('user', { reload: true });
+  }),
+
+  userList: computed('getTeam', function () {
+    if (this.getTeam) {
+      return this.allUsers.filterBy('team', this.getTeam);
+    } else {
+      return get(this,'store').findAll('user', { reload: true });
+    }
+  }),
+
+  myTeamMembers: computed('allUsers.isFulfilled', function () {
+    if (this.allUsers.isFulfilled) {
+      return this.allUsers.uniqBy('team');
+    }
+  }),
+
+  actions: {
+    touchEnd: function () {
+      this.toggleProperty('isShowingBody');
+      this.toggleProperty('clicked');
+    },
+
+    isFilter: async function (team) {
+      set(this,'getTeam', team);
+    },
+  },
 });
